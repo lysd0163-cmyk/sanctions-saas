edbf0de63c6f77c44c76b904ce6687ab7bdbb5b6
lysd0163-cmyk <lysd0163@gmail.com>
2026-06-29 18:30:16 +0300

    Refactor SQL queries for user statistics

--- admin.py
+++ admin.py
@@ -12,21 +12,24 @@ def admin_dashboard():
     conn = db.get_conn()
 
     active_users = conn.execute("""
-SELECT COUNT(DISTINCT user_id)
-FROM screening_logs
-""").fetchone()[0]
+        SELECT COUNT(DISTINCT user_id)
+        FROM screening_logs
+    """).fetchone()[0]
 
-returning_users = conn.execute("""
-SELECT COUNT(*)
-FROM (
-    SELECT user_id
-    FROM screening_logs
-    GROUP BY user_id
-    HAVING COUNT(*) > 1
-)
-""").fetchone()[0]
-    total_screenings = conn.execute("SELECT COUNT(*) FROM screening_logs").fetchone()[0]
-    
+    returning_users = conn.execute("""
+        SELECT COUNT(*)
+        FROM (
+            SELECT user_id
+            FROM screening_logs
+            GROUP BY user_id
+            HAVING COUNT(*) > 1
+        )
+    """).fetchone()[0]
+
+    total_screenings = conn.execute(
+        "SELECT COUNT(*) FROM screening_logs"
+    ).fetchone()[0]
+
     active_subscriptions = conn.execute(
         "SELECT COUNT(*) FROM users WHERE subscription_status='active'"
     ).fetchone()[0]
@@ -37,8 +40,6 @@ FROM (
         "admin.html",
         active_users=active_users,
         returning_users=returning_users,
-
         total_screenings=total_screenings,
-        
         active_subscriptions=active_subscriptions,
-  )
+    )
